855072ba029d1647d15f0f93b4b270b3100a60b3
InSlough <vladimir.reyt@gmail.com>
2018-12-26 19:43:11 +0300

    crispwp

---
+++ startit/header.autosave.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+ <head>
+        <!-- Meta Tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <meta name="description" content="Carrby - Agency Template">
+        <meta name="author" content="">
+
+        <!-- Page Title -->
+        <title>Carrby - Agency Template</title>
+
+        <!-- Favicon and Touch Icons -->
+        <link href="wp-content/themes/startit/images/favicon.png" rel="shortcut icon" type="image/png">
+
+        <!-- Lead Style -->
+        <link href="wp-content/themes/startit/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
+
+		<header id="header" class="header">
+            <div class="navigation">
+                <div class="container">
+                    <nav id="flexmenu">
+                        <div class="logo">
+                            <a href="index.html"><img src="wp-content/themes/startit/images/logo.png" alt="logo"></a>
+                        </div>
+                        <div class="nav-inner">
+                            <div id="mobile-toggle" class="mobile-btn"></div>
+                            <ul class="main-menu">
+                                <li class="menu-item"><a class="active" href="index.html#slider">Home</a></li>
+                                <li class="menu-item"><a href="index.html#services">Services</a></li>
+                                <li class="menu-item"><a href="index.html#about">About Us</a></li>
+                                <li class="menu-item"><a href="index.html#works">Portfolio</a></li>
+                                <li class="menu-item"><a href="index.html#blog">Blog</a></li>
+                                <li class="menu-item"><a href="index.html#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+	<div id="content" class="site-content">
